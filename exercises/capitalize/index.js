d7bbe9c560e5c01a107881b20ac7143c8938741a
aElian01 <eliana.sur@gmail.com>
2019-11-06 12:58:43 -0800

    capitalizing each word of a sentence using alternative appraoch

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,9 +6,11 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
-// o: capitalized string i: string c: time complexity 
+// o: capitalized string i: string c: language? time complexity? 
 
 function capitalize(str) {
+//wouldn't work for other languages potentially, like Spanish 
+
     let result = str[0].toUpperCase();
 
     for (let i = 1; i < str.length; i++) {

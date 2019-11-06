6388bab9bf8e1d6e3c6026cdb92fec728967f413
aElian01 <eliana.sur@gmail.com>
2019-11-05 23:05:58 -0800

    implementation for fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -11,7 +11,23 @@
 //   fizz
 //   4
 //   buzz
+//o: console log int from 1 to n, 'fizz' for multiples of 3, 'buzz' for multiples of 5, 'fizzbuzz' if both
+//i: int c: none
 
-function fizzBuzz(n) {}
+
+function fizzBuzz(n) {
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            console.log('fizzbuzz');
+        } else if (i % 3 === 0) {
+            console.log('fizz');
+        } else if (i % 5 === 0) {
+            console.log('buzz');
+        } else {
+            console.log(i);
+        }
+    }
+    
+}
 
 module.exports = fizzBuzz;

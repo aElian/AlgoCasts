51a3b3a68a9b3fd87ca1aebcca378c7cc369b8cd
aElian01 <eliana.sur@gmail.com>
2019-11-06 12:18:09 -0800

    implementation to check if two strings are anagrams using sort method

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -11,28 +11,36 @@
 //c: only chars, no punctuation marks or spaces, treat capital letters and lower case letters as equal
 
 function anagrams(stringA, stringB) {
-
-    const charMapA = buildCharMap(stringA);
-    const charMapB = buildCharMap(stringB);
-
-    if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
-        return false;
-    }
-
-    for (let key in charMapA) {
-        if (charMapB[key] !== charMapA[key]) {
-            return false;
-        }
-    }
-    return true;
+    return cleanString(stringA) === cleanString(stringB);
 }
 
-function buildCharMap(str) {
-    const charMap = {};
-    for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
-        charMap[char] = charMap[char] + 1 || 1;
-    }
-    return charMap;
+function cleanString(str) {
+    return str.replace(/[^\w]/g, '').toLowerCase().split('').sort().join('');
 }
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+
+//     const charMapA = buildCharMap(stringA);
+//     const charMapB = buildCharMap(stringB);
+
+//     if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+//         return false;
+//     }
+
+//     for (let key in charMapA) {
+//         if (charMapB[key] !== charMapA[key]) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// function buildCharMap(str) {
+//     const charMap = {};
+//     for (let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+//         charMap[char] = charMap[char] + 1 || 1;
+//     }
+//     return charMap;
+// }

7611df7fa206533d4eba5d248c6388ed8dbab10c
aElian01 <eliana.sur@gmail.com>
2019-11-05 22:43:27 -0800

    implementation for finding most occuring char in a string

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -4,7 +4,22 @@
 // --- Examples
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
+//o:char i:string c:case sensitive
 
-function maxChar(str) {}
+function maxChar(str) {
+    let chars = {};
+    let max = 0;
+    let maxChar = '';
+    for (let char of str) {
+        chars[char] = chars[char] + 1 || 1;
+    }
+    for (let char in chars) {
+        if (chars[char] > max) {
+            max = chars[char];
+            maxChar = char;
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;

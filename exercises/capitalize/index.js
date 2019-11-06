e8109a7d715b45c917c7725063b57f5e948a93bb
aElian01 <eliana.sur@gmail.com>
2019-11-06 12:40:56 -0800

    implementation to capitalize first letter of a string of words separated by a space using a for loop

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -6,7 +6,16 @@
 //   capitalize('a short sentence') --> 'A Short Sentence'
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
+// o: capitalized string i: string c: time complexity 
 
-function capitalize(str) {}
+function capitalize(str) {
+    let capitalized = [];
+    for (let word of str.split(' ')) {
+        capitalized.push(word[0].toUpperCase() + word.slice(1))
+    }
+    return capitalized.join(' ');
+}
+
+capitalize('a short sentence');
 
 module.exports = capitalize;

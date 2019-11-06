a1b91aaaed3de9155d97e8984468fb965d760527
aElian01 <eliana.sur@gmail.com>
2019-11-05 22:20:07 -0800

    implementation for reversing an integer

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -7,7 +7,13 @@
 //   reverseInt(500) === 5
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
+//o:int i:int c:work for negative numbers 
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let int = 0;
+    let reversedStr = Math.abs(n).toString().split('').reverse().join('');
+    int = parseInt(reversedStr);
+    return Math.sign(n) * int;
+}
 
 module.exports = reverseInt;

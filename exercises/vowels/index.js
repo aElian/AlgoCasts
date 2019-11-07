9262d0e4abc075354f1ce0fd409129f93a18ba81
aElian01 <eliana.sur@gmail.com>
2019-11-06 18:54:48 -0800

    implementation for checking vowel count in a string using includes method

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -6,7 +6,18 @@
 //   vowels('Hi There!') --> 3
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
+//o:integer i:string c: include both capital and lowercase vowels
 
-function vowels(str) {}
+function vowels(str) {
+    str = str.toLowerCase();
+    let count = 0;
+    let checker = ['a', 'e', 'i', 'o', 'u'];
+    for (let char of str) {
+        if (checker.includes(char)) {
+            count ++
+        }
+    }
+    return count;
+}
 
 module.exports = vowels;

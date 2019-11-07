93139e52a4484d5c6195e7cba81e40cbac629967
aElian01 <eliana.sur@gmail.com>
2019-11-06 17:56:35 -0800

    iterative solution for console logging pyramid shape with n levels

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -4,16 +4,43 @@
 // with N levels using the # character.  Make sure the
 // pyramid has spaces on both the left *and* right hand sides
 // --- Examples
-//   pyramid(1)
+//   pyramid(1) 1
 //       '#'
-//   pyramid(2)
+//   pyramid(2) 3
 //       ' # '
 //       '###'
-//   pyramid(3)
+//   pyramid(3) 5
 //       '  #  '
 //       ' ### '
 //       '#####'
+//   pyramid(4)  7
+//  '---#---'
+//  '--###--'   
+//  '-#####-'
+//  '#######'
+//   pyramid(5)  9
+//'----#----'
+//'---###---'
+//'--#####--'
+//'-#######-'
+//'#########'
 
-function pyramid(n) {}
+//o: console log pyramid shape using '#'  i:int c:iterative and recursive solution
+
+
+function pyramid(n) {
+    const midpoint = Math.floor((n * 2 - 1)/2);
+    for (let row = 0; row < n; row++) {
+        let level = '';
+        for (let column = 0; column < n * 2 - 1; column++) {
+            if (midpoint - row <= column && midpoint + row >= column) {
+                level += '#';
+            } else {
+                level += ' '
+            }
+        }
+        console.log(level);
+    }
+}
 
 module.exports = pyramid;
